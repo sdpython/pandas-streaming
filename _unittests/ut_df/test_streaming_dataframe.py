bd96853c86c0aed1b1bd5b347216be4094d0db45
xavier dupré <xavier.dupre@gmail.com>
2017-09-23 03:03:52 +0200

    add classic methods apply, applymap...

--- _unittests/ut_df/test_streaming_dataframe.py
+++ _unittests/ut_df/test_streaming_dataframe.py
@@ -61,6 +61,78 @@ class TestStreamingDataFrame(ExtTestCase):
         self.assertRaise(lambda: sdf.sort_values(
             "r"), StreamingInefficientException)
 
+    def test_to_csv(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        sdf = dummy_streaming_dataframe(100)
+        st = sdf.to_csv()
+        self.assertStartsWith(",cint,cstr\n0,0,s0", st)
+        
+    def test_head(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        sdf = dummy_streaming_dataframe(100)
+        st = sdf.head()
+        self.assertEqual(st.shape, (5, 2))
+        st = sdf.head(n=20)
+        self.assertEqual(st.shape, (20, 2))
+
+    def test_where(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        sdf = dummy_streaming_dataframe(100)
+        cols = sdf.columns
+        self.assertEqual(list(cols), ['cint', 'cstr'])
+        dts = sdf.dtypes
+        self.assertEqual(len(dts), 2)
+        res = sdf.where(lambda row: row["cint"] == 1)
+        st = res.to_csv()
+        self.assertStartsWith(",cint,cstr\n0,,\n1,1.0,s1", st)
+
+    def test_dataframe(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+            
+        sdf = dummy_streaming_dataframe(100)
+        df = sdf.to_dataframe()
+        self.assertEqual(df.shape, (100, 2))
+
+    def test_sample(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        sdf = dummy_streaming_dataframe(100)
+        res = sdf.sample(frac=0.1)
+        self.assertLesser(res.shape[0], 30)
+
+    def test_apply(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        sdf = dummy_streaming_dataframe(100)
+        self.assertNotEmpty(list(sdf))
+        sdf = sdf.applymap(str)
+        self.assertNotEmpty(list(sdf))
+        sdf = sdf.apply(lambda row: row["cint"] + "r", axis=1)
+        self.assertNotEmpty(list(sdf))
+        text = sdf.to_csv()
+        self.assertStartsWith("0,0r\n1,1r\n2,2r\n3,3r", text)
+        
 
 if __name__ == "__main__":
     unittest.main()
--- src/pandas_streaming/df/dataframe.py
+++ src/pandas_streaming/df/dataframe.py
@@ -4,12 +4,21 @@
 @brief Defines a streming dataframe.
 """
 import pandas
+from io import StringIO
 from ..exc import StreamingInefficientException
 
 
 class StreamingDataFrame:
     """
     Defines a streaming dataframe.
+
+    The constructor cannot receive an iterator otherwise
+    this class would be able to walk through the data
+    only once. Instead, it takes a function which generates
+    an iterator on :epkg:`pandas:DataFrame`.
+    Most of the functions returns either :epkg:`pandas:DataFrame`
+    or a @see cl StreamingDataFrame. In the second case,
+    methods can be chained.
     """
 
     def __init__(self, iter_creation):
@@ -17,11 +26,6 @@ class StreamingDataFrame:
         Wraps a iterator on dataframe.
 
         @param      iter_creation   code which creates an iterator.
-
-        The constructor cannot receive an iterator otherwise
-        this class would be able to walk through the data
-        only once. Instead, it takes a function which generates
-        an iterator on :epkg:`pandas:DataFrame`.
         """
         self.iter_creation = iter_creation
 
@@ -78,10 +82,127 @@ class StreamingDataFrame:
     def shape(self):
         """
         This is the kind of operations you do not want to do
-        when a file is large.
+        when a file is large because it goes through the whole
+        stream just to get the number of rows.
         """
         nl, nc = 0, 0
         for it in self:
             nc = max(it.shape[1], nc)
             nl += it.shape[0]
         return nl, nc
+        
+    @property
+    def columns(self):
+        """
+        See :epkg:`pandas:DataFrame:columns`.
+        """
+        for it in self:
+            return it.columns
+
+    @property
+    def dtypes(self):
+        """
+        See :epkg:`pandas:DataFrame:dtypes`.
+        """
+        for it in self:
+            return it.dtypes
+
+    def to_csv(self, path_or_buf=None, **kwargs):
+        """
+        Saves the dataframe into string.
+        See :epkg:`pandas:DataFrame.to_csv`.
+        """
+        if path_or_buf is None:
+            st = StringIO()
+            close = False            
+        elif isinstance(path_or_buf):
+            st = open(path_or_buf, "w", encoding=kwargs.get('encoding'))
+            close = True
+            
+        for df in self:
+            df.to_csv(st, **kwargs)
+            kwargs['header'] = False
+        
+        if close:
+            st.close()
+        if isinstance(st, StringIO):
+            return st.getvalue()
+        else:
+            return path_or_buf
+            
+    def to_dataframe(self):
+        """
+        Converts everything into a single dataframe.
+        """
+        return pandas.concat(self, axis=0)
+        
+    def iterrows(self):
+        """
+        See :epkg:`pandas:DataFrame:iterrows`.
+        """
+        for df in self:
+            for it in df.iterrows():
+                yield it
+
+    def head(self, n=5):
+        """
+        Returns the first rows as a DataFrame.
+        """
+        st = []
+        for df in self:
+            h = df.head(n=n)
+            st.append(h)
+            if h.shape[0] >= n:
+                break
+            n -= h.shape[0]
+        if len(st) == 1:
+            return st[0]
+        else:
+            return pandas.concat(st, axis=0)
+        
+    def tail(self, n=5):
+        """
+        Returns the last rows as a DataFrame.
+        The size of chunks must be greater than ``n`` to
+        get ``n`` lines. This method is not efficient
+        because the whole dataset must be walked through.
+        """        
+        for df in self:
+            h = df.tail(n=n)        
+        return h
+        
+    def where(self, *args, **kwargs):
+        """
+        Applies :epkg:`pandas:DataFrame:where`.
+        *inplace* must be False.
+        This function returns a @see cl StreamingDataFrame.
+        """
+        kwargs['inplace'] = False
+        return StreamingDataFrame(lambda: map(lambda df:df.where(*args, **kwargs), self))
+
+    def sample(self, **kwargs):
+        """
+        See :epkg:`pandas:DataFrame:sample`.
+        Only *frac* is available, otherwise choose
+        @see me reservoir_sampling.
+        This function returns a @see cl StreamingDataFrame.
+        """
+        if 'n' in kwargs:
+            raise ValueError('Only frac is implemented.')
+        return StreamingDataFrame(lambda: map(lambda df:df.sample(**kwargs), self))
+        
+    def apply(self, *args, **kwargs):
+        """
+        Applies :epkg:`pandas:DataFrame:apply`.
+        This function returns a @see cl StreamingDataFrame.
+        """
+        return StreamingDataFrame(lambda: map(lambda df:df.apply(*args, **kwargs), self))
+
+    def applymap(self, *args, **kwargs):
+        """
+        Applies :epkg:`pandas:DataFrame:applymap`.
+        This function returns a @see cl StreamingDataFrame.
+        """
+        return StreamingDataFrame(lambda: map(lambda df:df.applymap(*args, **kwargs), self))
+
+                
