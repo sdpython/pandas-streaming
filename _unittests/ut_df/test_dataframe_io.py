d968da98f8c17fdf60ba4eb822236d17b1fdb8d1
xavier dupré <xavier.dupre@gmail.com>
2017-12-03 19:18:25 +0100

    extend to_zip and read_zip for numpy arrays

--- _unittests/ut_df/test_dataframe_io.py
+++ _unittests/ut_df/test_dataframe_io.py
@@ -9,6 +9,7 @@ import unittest
 import pandas
 import io
 import zipfile
+import numpy
 
 
 try:
@@ -48,7 +49,7 @@ from src.pandas_streaming.df import to_zip, read_zip
 
 class TestDataFrameIO(ExtTestCase):
 
-    def test_zip(self):
+    def test_zip_dataframe(self):
         fLOG(
             __file__,
             self._testMethodName,
@@ -76,6 +77,32 @@ class TestDataFrameIO(ExtTestCase):
         df3 = read_zip(zp, encoding='utf-8')
         zp.close()
         self.assertEqualDataFrame(df, df3)
+
+    def test_zip_numpy(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        df = numpy.zeros((3, 4))
+        df[2, 3] = 1
+
+        temp = get_temp_folder(__file__, "temp_zip")
+        name = os.path.join(temp, "df.zip")
+        to_zip(df, name, "arr.npy")
+        df2 = read_zip(name, "arr.npy")
+        self.assertEqualArray(df, df2)
+
+        st = io.BytesIO()
+        zp = zipfile.ZipFile(st, 'w')
+        to_zip(df, zp, "arr.npy")
+        zp.close()
+
+        st = io.BytesIO(st.getvalue())
+        zp = zipfile.ZipFile(st, 'r')
+        df3 = read_zip(zp, "arr.npy")
+        zp.close()
+        self.assertEqualArray(df, df3)
 
 
 if __name__ == "__main__":
--- src/pandas_streaming/df/dataframe_io.py
+++ src/pandas_streaming/df/dataframe_io.py
@@ -5,8 +5,9 @@
 """
 import io
 import os
-import pandas
 import zipfile
+import pandas
+import numpy
 
 
 def to_zip(df, zipfilename, zname="df.csv", **kwargs):
@@ -14,17 +15,18 @@ def to_zip(df, zipfilename, zname="df.csv", **kwargs):
     Saves a dataframe into a zip file.
     It can be read by @see fn to_zip.
 
-    @param      df          dataframe
+    @param      df          dataframe or numpy array
     @param      zipfilename a :epkg:`*py:zipfile:ZipFile` or a filename
     @param      zname       a filename in th zipfile
-    @param      kwargs      parameters for :epkg:`pandas:to_csv`
+    @param      kwargs      parameters for :epkg:`pandas:to_csv` or
+                            :epkg:`numpy:save`
     @return                 zipfilename
 
     .. exref::
         :title: Saves and read a dataframe in a zip file
 
         This shows an example on how to save and read a
-        dataframe directly into a zip file.
+        :epkg:`pandas:dataframe` directly into a zip file.
 
         .. runpython::
             :showcode:
@@ -35,12 +37,46 @@ def to_zip(df, zipfilename, zname="df.csv", **kwargs):
             df = pandas.DataFrame([dict(a=1, b="e"),
                                    dict(b="f", a=5.7)])
 
+            name = "dfs.zip"
             to_zip(df, name, encoding="utf-8", index=False)
             df2 = read_zip(name, encoding="utf-8")
             print(df2)
+
+    .. exref::
+        :title: Saves and read a numpy array in a zip file
+
+        This shows an example on how to save and read a
+        :epkg:`numpy:ndarray` directly into a zip file.
+
+        .. runpython::
+            :showcode:
+
+            import pandas
+            from pandas_streaming.df import to_zip, read_zip
+
+            arr = numpy.array([[0.5, 1.5], [0.4, 1.6]])
+
+            name = "dfs.zip"
+            to_zip(df, name, 'arr.npy')
+            arr2 = read_zip(name, 'arr.npy')
+            print(arr2)
     """
-    stb = io.StringIO()
-    df.to_csv(stb, **kwargs)
+    if isinstance(df, pandas.DataFrame):
+        stb = io.StringIO()
+        ext = os.path.splitext(zname)[-1]
+        if ext == '.npy':
+            raise ValueError(
+                "Extension '.npy' cannot be used to save a dataframe.")
+        df.to_csv(stb, **kwargs)
+    elif isinstance(df, numpy.ndarray):
+        stb = io.BytesIO()
+        ext = os.path.splitext(zname)[-1]
+        if ext != '.npy':
+            raise ValueError(
+                "Extension '.npy' is required when saving a numpy array.")
+        numpy.save(stb, df, **kwargs)
+    else:
+        raise TypeError("Type not handled {0}".format(type(df)))
     text = stb.getvalue()
 
     if isinstance(zipfilename, str):
@@ -88,7 +124,11 @@ def read_zip(zipfilename, zname="df.csv", **kwargs):
 
     content = zf.read(zname)
     stb = io.BytesIO(content)
-    df = pandas.read_csv(stb, **kwargs)
+    ext = os.path.splitext(zname)[-1]
+    if ext == '.npy':
+        df = numpy.load(stb, **kwargs)
+    else:
+        df = pandas.read_csv(stb, **kwargs)
 
     if close:
         zf.close()
