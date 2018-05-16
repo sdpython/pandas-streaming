738a0f91d666dc9cc179dd9fa0260b82b4651a1b
xavier dupré <xavier.dupre@gmail.com>
2018-05-17 01:01:31 +0200

    update circleci and setup

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -2,25 +2,14 @@
 import sys
 import os
 import sphinx_rtd_theme
+from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "..",
-            "..",
-            "..",
-            "pyquickhelper",
-            "src")))
 
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 set_sphinx_variables(__file__, "pandas_streaming", "Xavier Dupré", 2018,
                      "sphinx_rtd_theme", [
                          sphinx_rtd_theme.get_html_theme_path()],
