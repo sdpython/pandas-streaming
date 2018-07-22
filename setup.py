e65a2ab34588ea445d48b1fe453b5adefe60b8c6
xavier dupré <xavier.dupre@gmail.com>
2018-07-22 17:33:38 +0200

    update CI

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ if is_local():
         layout=["html"], github_owner='sdpython',
         add_htmlhelp=sys.platform.startswith("win"),
         coverage_options=dict(omit=["*exclude*.py"]),
-        fLOG=logging_function, covtoken=("14c7930a-a5c0-405d-a22f-3f9c6feaf0bc", "'_UT_36_std' in outfile"))
+        fLOG=logging_function, covtoken=("14c7930a-a5c0-405d-a22f-3f9c6feaf0bc", "'_UT_37_std' in outfile"))
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
                        "upload_docs", "bdist_wininst", "build_ext"} & set(sys.argv)):
